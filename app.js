395c6ae2d27e692dda3b33ad26d280c744ffb3b8
scheng0718 <scheng0718@gmail.com>
2023-07-17 12:03:58 -0700

    feat: add mongoose and define data models and schema

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const express = require('express')
+const mongoose = require('mongoose')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const app = express()
+mongoose.connect(process.env.MONGODB_URI)
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 const port = 3000
 
 app.use('/', (req, res) => {
---
+++ models/record.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const recordSchema = new Schema ({
+  id: {
+    type: Number,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true 
+  },
+  amount: {
+    type: Number,
+    required: true
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    index: true,
+    required: true
+  },
+  category: {
+    type: Schema.Types.ObjectId,
+    ref: 'Category',
+    index: true,
+    required: true
+  }
+})
+
+module.exports = mongoose.model('Record', recordSchema)
