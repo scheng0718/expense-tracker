ec3b712925a1d8751c8c3a9701548ce13a052012
scheng0718 <scheng0718@gmail.com>
2023-07-20 22:53:39 -0700

    feat: add post route for user registration

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcryptjs')
 const Record = require('../../models/record')
 const Category = require('../../models/category')
 const User = require('../../models/user')
@@ -10,6 +11,34 @@ router.get('/login', (req, res) => {
 
 router.get('/register', (req, res) => {
   res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  User.findOne({ email })
+    .lean()
+    .then(user => {
+      if (user) {
+        console.log('User has been registered!')
+        return res.render('register', {
+          name,
+          email,
+          password,
+          confirmPassword
+        })
+      }
+      return bcrypt
+        .genSalt(10)
+        .then(salt => bcrypt.hash(password, salt))
+        .then(hash => User.create({
+            name,
+            email,
+            password: hash
+          })
+            .then(() => res.redirect('/'))
+            .catch(error => console.log(error)))   
+    })
+    .catch(error => console.log(error))
 })
 
 module.exports = router
