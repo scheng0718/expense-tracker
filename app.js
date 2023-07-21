fc737648d1c843a48c19e25b1d8cd18f112c2652
scheng0718 <scheng0718@gmail.com>
2023-07-20 23:01:52 -0700

    feat: add express-session

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const { engine }  = require('express-handlebars')
 const app = express()
 const methodOverride = require('method-override')
+const session = require('express-session')
 const router = require('./routes')
 require('./config/mongoose')
 const port = 3000
@@ -11,6 +12,11 @@ app.set('view engine', 'hbs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(router)
 
 app.listen(port, () => {
